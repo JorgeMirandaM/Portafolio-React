449318b132cd665ba4d44329f9e1099f073d3cce
JorgeMirandaM <jorge100099@hotmail.com>
2023-12-27 19:59:08 -0600

    hook useForm

--- src/components/contactSection/ContactSection.jsx
+++ src/components/contactSection/ContactSection.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import './contactSection.css';
 import contactImage from '../../assets/contacto/contacto.png'
+import { useForm } from '../../hooks/useForm';
 
 export const ContactSection = () => {
+
+    const { formState, onInputChange } = useForm({
+        name: "",
+        email: "",
+        topic: "",
+        message: ""
+    });
+
+    const { name,
+        email,
+        topic,
+        message } = formState;
+
     return (
         <>
-            <div className="container-fluid contactSection py-5 mb-5">
+            <div className="container-fluid contactSection py-5 " id='contacto'>
                 <div className="container py-5">
                     <div className="row">
                         <div className="d-none d-lg-block col-lg-5">
@@ -22,25 +36,25 @@ export const ContactSection = () => {
                             </div>
                             <form action="" className='d-flex flex-column gap-4 contactForm'>
                                 <div className="form-floating">
-                                    <input type="text" className='form-control' id='inputNombre' placeholder='Nombre' aria-describedby="validationNombre" required />
+                                    <input type="text" className='form-control' id='inputNombre' placeholder='Nombre' aria-describedby="validationNombre" name='name' value={name} onChange={onInputChange} required />
                                     <label htmlFor="inputNombre">Nombre</label>
                                     <div id='validationNombre' className="invalid-feedback">
                                         Please choose a username.
                                     </div>
                                 </div>
                                 <div className="form-floating">
-                                    <input type="email" className='form-control' id='inputEmail' placeholder='email@gmail.com' aria-describedby="validationEmail" required />
+                                    <input type="email" className='form-control' id='inputEmail' placeholder='email@gmail.com' aria-describedby="validationEmail" name='email' value={email} onChange={onInputChange} required />
                                     <label htmlFor="inputEmail">Correo</label>
                                     <div id='validationEmail' className="invalid-feedback">
                                         Please choose a username.
                                     </div>
                                 </div>
                                 <div className="form-floating">
-                                    <input type="text" className='form-control' id='inputAsunto' placeholder='Asunto' required />
+                                    <input type="text" className='form-control' id='inputAsunto' placeholder='Asunto' name='topic' value={topic} onChange={onInputChange} required />
                                     <label htmlFor="inputAsunto">Asunto</label>
                                 </div>
                                 <div className="form-floating">
-                                    <textarea className="form-control" placeholder="Mensaje" id="textareaMensaje" required></textarea>
+                                    <textarea className="form-control" placeholder="Mensaje" id="textareaMensaje" name='message' value={message} onChange={onInputChange} required></textarea>
                                     <label htmlFor="textareaMensaje">Mensaje</label>
                                 </div>
                                 <div>
